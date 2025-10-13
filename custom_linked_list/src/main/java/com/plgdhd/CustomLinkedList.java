6d6bb215d402cef7c7e691f8a8b86a9b3d6dc1ac
plgdhd <aivanuk161@gmail.com>
2025-10-14 00:15:26 +0300

    task_1 after review

--- custom_linked_list/src/main/java/com/plgdhd/CustomLinkedList.java
+++ custom_linked_list/src/main/java/com/plgdhd/CustomLinkedList.java
@@ -2,19 +2,79 @@ package com.plgdhd;
 
 import java.util.NoSuchElementException;
 
-import lombok.Data;
+public class CustomLinkedList<T> implements CustomList<T> {
 
-@Data
-public class CustomLinkedList<T> {
+    public class Node<T> {
+
+        private T data;
+        private Node<T> next;
+        private Node<T> prev;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public Node<T> getPrev() {
+            return prev;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        public void setPrev(Node<T> prev) {
+            this.prev = prev;
+        }
+    }
 
     private Node<T> head;
     private Node<T> tail;
     private long size;
 
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public void setHead(Node<T> head) {
+        this.head = head;
+    }
+
+    public Node<T> getTail() {
+        return tail;
+    }
+
+    public void setTail(Node<T> tail) {
+        this.tail = tail;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    @Override
     public long size() {
         return size;
     }
 
+    @Override
     public void addFirst(T el) {
         Node<T> newHead = new Node<>(el);
         if (head == null) {
@@ -28,6 +88,7 @@ public class CustomLinkedList<T> {
         size++;
     }
 
+    @Override
     public void addLast(T el) {
         Node<T> newTail = new Node<>(el);
         if (tail == null) {
@@ -41,6 +102,7 @@ public class CustomLinkedList<T> {
         size++;
     }
 
+    @Override
     public void add(int index, T el) {
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException();
@@ -68,18 +130,21 @@ public class CustomLinkedList<T> {
         size++;
     }
 
+    @Override
     public T getFirst() {
         if (head == null)
             throw new NoSuchElementException("The first element doesn't exist");
         return head.getData();
     }
 
+    @Override
     public T getLast() {
         if (tail == null)
             throw new NoSuchElementException("The last element doesn't exist");
         return tail.getData();
     }
 
+    @Override
     public T get(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
@@ -91,6 +156,7 @@ public class CustomLinkedList<T> {
         return buffer.getData();
     }
 
+    @Override
     public T removeFirst() {
         if (head == null) {
             throw new NoSuchElementException("The first element doesn't exist");
@@ -106,6 +172,7 @@ public class CustomLinkedList<T> {
         return savedData;
     }
 
+    @Override
     public T removeLast() {
         if (tail == null) {
             throw new NoSuchElementException("The last element doesn't exist");
@@ -121,13 +188,16 @@ public class CustomLinkedList<T> {
         return savedData;
     }
 
+    @Override
     public T remove(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
 
-        if (index == 0) return removeFirst();
-        if (index == size - 1) return removeLast();
+        if (index == 0)
+            return removeFirst();
+        if (index == size - 1)
+            return removeLast();
 
         Node<T> buffer = head;
         for (int i = 0; i < index; i++) {
