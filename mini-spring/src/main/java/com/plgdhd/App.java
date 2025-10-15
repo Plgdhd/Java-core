1d9f8e31674c41a1009237dc8c1aa599a2cef1cf
plgdhd <aivanuk161@gmail.com>
2025-10-16 02:32:53 +0300

    deleted task 4 from main

--- mini-spring/src/main/java/com/plgdhd/App.java
+++
@@ -1,9 +0,0 @@
-package com.plgdhd;
-
-public class App {
-    public static void main(String[] args) {
-        ApplicationContext context = new ApplicationContext("com.plgdhd");
-        CoronaDesinfector disinfector = context.getBean(CoronaDesinfector.class);
-        disinfector.start(new Room());
-    }
-}
--- mini-spring/src/main/java/com/plgdhd/interfaces/Announcer.java
+++
@@ -1,5 +0,0 @@
-package com.plgdhd.interfaces;
-
-public interface Announcer {
-    void announce(String message);
-}
